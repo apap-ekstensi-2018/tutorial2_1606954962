1e75354c77596bacec8c0a7a99e8108b66b6d9be
rahmatchaniago <rahmatchaniago28@gmail.com>
2018-02-24 23:37:37 +0700

    tutorial 2

---
+++ Tutorial2_1606954962/src/main/java/com/example/demo/Tutorial22Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tutorial22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tutorial22Application.class, args);
+	}
+}
